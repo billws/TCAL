f25288bb9be6d169272b55553ee450f19d1dd375
billws <billws@gmail.com>
2020-12-23 14:30:05 +0800

    document page

--- src/js/reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-
-import calReducer from "./app/reducers/calreducer";
-
-const rootReducer = combineReducers({
-  calculator: calReducer
-})
-
-export default rootReducer
